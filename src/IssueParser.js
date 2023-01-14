1eaf71b3d76d65de536d389b614e225682353499
Kiriushkin Dmitrii <kiriushkindmitrii@gmail.com>
2023-01-14 15:03:47 +0300

    Implement hours ratio calc and summarizing

---
+++ src/IssueParser.js
@@ -0,0 +1,163 @@
+import fs from 'fs/promises';
+import config from './config.js';
+
+const {
+  component,
+  dateFrom,
+  scorePlan,
+  ratioPlan,
+  baseBonus,
+  hoursLimit,
+  isLimitHours,
+} = config;
+
+class IssueParser {
+  constructor(jira) {
+    this.jira = jira;
+    this.component = component;
+    this.dateFrom = dateFrom;
+    this.text = [];
+    this.issues = [];
+    this.score = 0;
+    this.ratio = 0;
+
+    this.text.push(`Команда: ${component}`);
+    this.text.push(
+      `Период: c ${new Date(
+        dateFrom
+      ).toLocaleDateString()} по ${new Date().toLocaleDateString()}`
+    );
+    this.insertLine();
+
+    this.text.push(`План задач: ${scorePlan}`);
+    this.text.push(`План коэффициент: ${ratioPlan}`);
+    this.text.push(`Базовая премия: ${baseBonus}`);
+
+    if (isLimitHours)
+      this.text.push(`Ограничение по часам одной задачи: ${hoursLimit}`);
+
+    this.insertLine();
+  }
+
+  async makeQuery(text, query, weight, targetHours) {
+    const resp = await this.jira.searchJira(query, {
+      maxResults: 1000,
+    });
+
+    this.text.push(`${text}: ${resp.total}`);
+
+    const score = weight ? resp.total * weight : null;
+
+    if (score) this.text.push(`  Баллов: ${score}`);
+
+    const hours = await this.calculateHours(resp.issues);
+
+    this.text.push(`  Средние часы: ${Math.round(hours * 100) / 100}`);
+
+    const ratio = targetHours ? targetHours / hours : null;
+
+    if (ratio)
+      this.text.push(`  коэффициент: ${Math.round(ratio * 100) / 100}`);
+
+    this.insertLine();
+
+    this.issues.push({ title: text, amount: resp.total, score, ratio });
+  }
+
+  insertLine() {
+    this.text.push('-'.repeat(25));
+  }
+
+  async calculateHours(issues) {
+    try {
+      const issueHours = [];
+
+      for (const issue of issues) {
+        const resp = await this.jira.findIssue(issue.key, 'changelog');
+
+        let spentHours = 0;
+
+        let fromDate = new Date(resp.fields.created);
+        let toDate;
+
+        for (const history of resp.changelog.histories) {
+          for (const item of history.items) {
+            if (
+              (item.field === 'status' && item.toString === 'Перевод') ||
+              item.toString === 'Финальная проверка' ||
+              item.toString === 'Требует уточнения'
+            ) {
+              toDate = new Date(history.created);
+
+              spentHours +=
+                (toDate.getTime() - fromDate.getTime()) / 1000 / 60 / 60;
+            }
+            if (
+              (item.field === 'status' && item.fromString === 'Перевод') ||
+              item.fromString === 'Финальная проверка' ||
+              item.fromString === 'Требует уточнения'
+            )
+              fromDate = new Date(history.created);
+          }
+        }
+
+        issueHours.push(spentHours);
+      }
+
+      const arr = isLimitHours
+        ? issueHours.filter((val) => hoursLimit > val)
+        : issueHours;
+
+      const allHours = arr.reduce((sum, val) => sum + val, 0);
+
+      const averageHours = allHours / arr.length;
+
+      return averageHours;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  sumUp() {
+    let score = 0,
+      ratio = 0,
+      amount = 0;
+
+    this.issues.forEach((issue) => {
+      if (issue.score) score += issue.score;
+      if (issue.ratio) (ratio += issue.ratio), amount++;
+    });
+
+    ratio = ratio / amount;
+
+    this.text.push(`Всего баллов: ${score}`);
+    this.text.push(`Общий коэффициент: ${Math.round(ratio * 100) / 100}`);
+    this.insertLine();
+
+    const scoreTotal = (score / scorePlan) * 0.6 * 100;
+    const ratioTotal = (ratio / ratioPlan) * 0.4 * 100;
+    const total = Math.round((scoreTotal + ratioTotal) * 100) / 100;
+    const bonus = Math.round(((baseBonus * total) / 100) * 100) / 100;
+
+    this.text.push(`Итого баллов: ${total}`);
+    this.text.push(`Итоговая сумма: ${bonus} (${bonus / 4})`);
+  }
+
+  async writeOutput() {
+    const filePath = `../out/${new Date().toLocaleDateString()}-${
+      this.component
+    }-${isLimitHours ? 'limited' : ''}.log`;
+
+    try {
+      await fs.access('../out');
+    } catch (err) {
+      await fs.mkdir('../out');
+    }
+
+    await fs.writeFile(filePath, this.text.join('\r\n'));
+
+    console.log(`Результаты доступны в ${filePath.replace('../', '')}`);
+  }
+}
+
+export default IssueParser;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import fs from 'fs/promises';
 import JiraApi from 'jira-client';
 import config from './config.js';
+import IssueParser from './IssueParser.js';
 
 const { username, password, component, dateFrom } = config;
 
+let jira;
+
 try {
-  let jira = new JiraApi({
+  jira = new JiraApi({
     protocol: 'https',
     host: 'jira.lucky-team.pro',
     username,
@@ -13,82 +16,50 @@ try {
     apiVersion: '2',
     strictSSL: true,
   });
-
-  const ALL_QUERY = `project = LAND AND status = Done AND component = ${component} AND resolved >= ${dateFrom} ORDER BY priority DESC, updated DESC`;
-
-  const NO_CHANGES_QUERY = `project = LAND AND status = Done AND component = ${component} AND resolved >= ${dateFrom} AND text ~ "Добавить преленд без каких либо изменений по контенту" ORDER BY priority DESC, updated DESC`;
-
-  const CURRENT_PROMO_QUERY = `project = LAND AND status = Done AND component = ${component} AND resolved >= ${dateFrom} AND text ~ "Изменение в текущем промо" ORDER BY priority DESC, updated DESC`;
-
-  const COPY_PROMO_QUERY = `project = LAND AND status = Done AND component = ${component} AND resolved >= ${dateFrom} AND text ~ "Копия промо с будущими изменениями" ORDER BY priority DESC, updated DESC`;
-
-  const GEO_CHARACTERS_QUERY = `project = LAND AND status = Done AND component = ${component} AND resolved >= ${dateFrom} AND text ~ "Добавить преленд и адаптировать под оффер, новое гео, персонажей" ORDER BY priority DESC, updated DESC`;
-
-  const text = [];
-
-  text.push(`Команда: ${component}`);
-  text.push(`Период: c ${dateFrom} по сейчас`);
-  text.push('-'.repeat(25));
-
-  const allResp = jira.searchJira(ALL_QUERY, {
-    maxResults: 1,
-  });
-
-  const noChangesResp = jira.searchJira(NO_CHANGES_QUERY, {
-    maxResults: 1,
-  });
-
-  const currentPromoResp = jira.searchJira(CURRENT_PROMO_QUERY, {
-    maxResults: 1,
-  });
-
-  const copyPromoResp = jira.searchJira(COPY_PROMO_QUERY, {
-    maxResults: 1,
-  });
-
-  const [
-    { total: allIssues },
-    { total: noChangesIssues },
-    { total: currentPromoIssues },
-    { total: copyPromoIssues },
-  ] = await Promise.all([
-    allResp,
-    noChangesResp,
-    currentPromoResp,
-    copyPromoResp,
-  ]);
-
-  text.push(`Всего задач: ${allIssues}`);
-  text.push(`Без изменений по контенту: ${noChangesIssues}`);
-  text.push(`Изменения в текущем промо: ${currentPromoIssues}`);
-  text.push(`Копия промо: ${copyPromoIssues}`);
-
-  const geoCharactersIssues =
-    allIssues - noChangesIssues - currentPromoIssues - copyPromoIssues;
-
-  text.push(`Добавить преленд и адаптировать: ${geoCharactersIssues}`);
-
-  text.push('-'.repeat(25));
-
-  const score =
-    noChangesIssues +
-    currentPromoIssues +
-    copyPromoIssues * 2 +
-    geoCharactersIssues * 3;
-
-  text.push(`Всего баллов: ${score}`);
-
-  const filePath = `../out/${new Date().toLocaleDateString()}.log`;
-
-  try {
-    await fs.access('../out');
-  } catch (err) {
-    await fs.mkdir('../out');
-  }
-
-  await fs.writeFile(filePath, text.join('\r\n'));
-
-  console.log(`Результаты доступны в ${filePath.replace('../', '')}`);
 } catch (error) {
   if (error.message.match('(401)')) console.log('Неверные данные от jira');
+  else console.error(error.message);
 }
+
+const textQueries = [
+  '',
+  'Добавить преленд без каких либо изменений по контенту',
+  'Изменение в текущем промо',
+  'Копия промо с будущими изменениями',
+  'Добавить преленд и адаптировать под другой оффер',
+];
+
+const [
+  allQuery,
+  noChangesQuery,
+  currentPromoQuery,
+  copyPromoQuery,
+  geoCharactersQuery,
+] = textQueries.map(
+  (query) =>
+    `project = LAND AND status = Done AND component = ${component} AND resolved >= ${dateFrom} ${
+      query ? ` AND text ~ "${query}" ` : ''
+    }ORDER BY priority DESC, updated DESC`
+);
+
+const issueParser = new IssueParser(jira);
+
+await issueParser.makeQuery('Всего задач', allQuery);
+await issueParser.makeQuery('Без изменений по контенту', noChangesQuery, 1, 11);
+await issueParser.makeQuery(
+  'Изменения в текущем промо',
+  currentPromoQuery,
+  1,
+  11
+);
+await issueParser.makeQuery('Копия промо', copyPromoQuery, 2, 22);
+await issueParser.makeQuery(
+  'Добавить и адаптировать под другой оффер',
+  geoCharactersQuery,
+  3,
+  33
+);
+
+issueParser.sumUp();
+
+issueParser.writeOutput();
